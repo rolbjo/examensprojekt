7e8ce8e309c8e9f06c6f9e92915156e3e150a6e9
Roland Björndal <bjorndahl95@hotmail.com>
2023-12-14 15:28:56 +0100

    fix on fileending

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
+import Carousel from './components/carousel'
 
 function App() {
   const [hej, setHej] = useState('')
@@ -24,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR...... testing
         </p>
       </div>
-
+      <Carousel />
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more.
       </p>
